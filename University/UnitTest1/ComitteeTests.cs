7cf344dcc60f6790f73d07a399f551e5c0eba91b
dburdak <dburdak@kse.org.ua>
2025-03-11 22:51:12 +0200

    exceptions in Evaluator and CommitteeMember classes"

--- University/UnitTest1/ComitteeTests.cs
+++ University/UnitTest1/ComitteeTests.cs
@@ -31,4 +31,27 @@ public class ComitteeTests
       string result = commiteemem.GiveFeedback(proj);
       Assert.Equal("Feedback for Creating new type of MRT", result);
    }
+   
+   [Fact]
+   public void TestingExceptionForResearchStruct()
+   {
+      var mockEvaluator = new Mock<IEvaluator>();
+      mockEvaluator.Setup(e => e.Proj_feedback(It.IsAny<ResearchProject1>())).Returns("Проблемка: Немає назви, немає проєкту, придумайте і тоді пишіть!");
+      var commiteemem = new CommitteeMember(mockEvaluator.Object,"Mark Levytskyi", "Senior Evaluation Manager");
+      var proj = new ResearchProject1();
+      Assert.Equal("Проблемка: Немає назви, немає проєкту, придумайте і тоді пишіть!", commiteemem.GiveFeedback(proj));
+            
+   }
+   [Fact]
+   public void TestingExceptionForEvaluatingProj()
+   {
+      var mockEvaluator = new Mock<IEvaluator>();
+      mockEvaluator.Setup(e => e.EvaluateProj(It.IsAny<ResearchProject1>(), It.IsAny<int>())).Returns("Проблема: Оцінка не може бути від'ємною");
+      var commiteemem = new CommitteeMember(mockEvaluator.Object,"Mark Levytskyi", "Senior Evaluation Manager");
+      var proj = new ResearchProject1();
+      proj.Field = "Medical Diagnostic";
+      proj.Label = "Creating new type of MRT";
+      Assert.Equal("Проблема: Оцінка не може бути від'ємною", commiteemem.GradeProj(proj, 52));
+            
+   }
 }
--- University/University/Evaluator.cs
+++ University/University/Evaluator.cs
@@ -4,11 +4,52 @@ public class Evaluator:IEvaluator
 {
     public string Proj_feedback(ResearchProject1 research_proj)
     {
-        return $"Feedback for {research_proj.Label}";
+        try
+        {
+            if (research_proj.Label == null)
+            {
+                throw new Exception("Немає назви, немає проєкту, придумайте і тоді пишіть!");
+            }
+
+            return $"Feedback for {research_proj.Label}";
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Проблемка: {e.Message}"); //логування
+            return $"Проблемка: {e.Message}";
+            
+        }
+        finally
+        {
+            Console.WriteLine("Помилка оброблена");
+        }
+        
     }
 
     public string EvaluateProj(ResearchProject1 research_proj, int score)
     {
-        return $"{research_proj.Label} received {score} grade";  
+        try
+        {
+            if (score < 0)
+            {
+                throw new ArgumentException("Оцінка не може бути від'ємною");
+            }
+
+            return $"{research_proj.Label} received {score} grade";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Проблема: {e.Message}");
+            throw;
+
+        }
+        finally
+        {
+            Console.WriteLine("Опрацювання проблеми завершене");
+        }
+         
+        
+        
     }
 }
